9f4f0ab94492860a7403c0693e6c9c7dd39e7f21
John Thompson <john@springframework.guru>
2024-04-13 14:58:18 -0400

    adding prompts and messages

--- src/main/java/guru/springframework/springairagexpert/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springairagexpert/services/OpenAIServiceImpl.java
@@ -5,8 +5,10 @@ import guru.springframework.springairagexpert.model.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
@@ -28,23 +30,40 @@ public class OpenAIServiceImpl implements OpenAIService {
     final ChatClient chatClient;
     final VectorStore vectorStore;
 
-    @Value("classpath:/templates/rag-prompt-template-meta.st")
+    @Value("classpath:/templates/rag-prompt-template.st")
     private Resource ragPromptTemplate;
+
+    @Value("classpath:/templates/system-message.st")
+    private Resource systemMessageTemplate;
 
     @Override
     public Answer getAnswer(Question question) {
+        PromptTemplate systemMessagePromptTemplate = new SystemPromptTemplate(systemMessageTemplate);
+        Message systemMessage = systemMessagePromptTemplate.createMessage();
+
         List<Document> documents = vectorStore.similaritySearch(SearchRequest
                 .query(question.question()).withTopK(5));
         List<String> contentList = documents.stream().map(Document::getContent).toList();
 
         PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
-        Prompt prompt = promptTemplate.create(Map.of("input", question.question(), "documents",
+        Message userMessage = promptTemplate.createMessage(Map.of("input", question.question(), "documents",
                 String.join("\n", contentList)));
 
-        contentList.forEach(System.out::println);
+        //contentList.forEach(System.out::println);
 
-        ChatResponse response = chatClient.call(prompt);
+        ChatResponse response = chatClient.call(new Prompt(List.of(systemMessage, userMessage)));
 
         return new Answer(response.getResult().getOutput().getContent());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
